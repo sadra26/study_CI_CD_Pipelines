3e8dd710cfd981576737e89b4f1d231019df52a3
sadra26 <alzastro12345@gmail.com>
2026-09-04 15:35:36 +0700

    revisi the validation

--- math.test.js
+++ math.test.js
@@ -1,6 +1,6 @@
 const sum = require("./math");
 
-test("The result must be 20 when calling sum(10, 10)", () => {
+test("The result must be 20 when giving sum(10, 10)", () => {
   const result = sum(10, 10);
   expect(result).toBe(20);
 });
